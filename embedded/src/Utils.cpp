f32c6c858929fa1ac38e52d40548d5e3bb207ad9
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-07-14 21:27:56 -0300

    (feat): formatted time stamp

---
+++ embedded/src/Utils.cpp
@@ -0,0 +1,24 @@
+#include <time.h>
+#include "utils.h"
+
+const char* getFormattedTimestamp() {
+    static char timestamp[20];
+    
+
+    time_t now = time(nullptr);
+    if (now < 0) {
+        return nullptr;
+    }
+
+    struct tm timeinfo;
+    if (localtime_r(&now, &timeinfo) == nullptr) {
+        return nullptr;
+    }
+    
+    if (timeinfo.tm_year < 120) { 
+        return nullptr; 
+    }
+    
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", &timeinfo);
+    return timestamp;
+}
