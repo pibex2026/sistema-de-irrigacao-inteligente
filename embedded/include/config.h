ce3da99dcf170dcce5512d346b3673a7d3883ff8
victorgiuliano2162 <vitor.giuliano@gmail.com>
2026-07-14 21:28:36 -0300

    (create): header file to handle config infos

---
+++ embedded/include/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Config {
+
+    namespace Pins {
+        constexpr uint8_t DHT_SENSOR_PIN = 4; 
+
+        constexpr uint8_t SOIL_MOISTURE_ADC = 32; 
+        constexpr uint8_t RELAY_VALVE = 25; 
+
+        //LED onboard
+        constexpr uint8_t STATUS_LED  = 2;  
+    }
+
+    namespace Hardware {
+        
+        #define DHT_SENSOR_TYPE DHT22 
+    }
+}
---
+++ embedded/include/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace Constants {
+    constexpr int ONE_SECOND_IN_MS = 1 * 1000;
+    constexpr int ONE_MINUTE_IN_MS = 1 * 60 * 1000;
+    constexpr int ONE_HOUR_IN_MS = 1 * 60 * 60 * 1000;
+    constexpr int ONE_DAY_IN_MS = 1 * 24 * 60 * 60 * 1000;
+}
