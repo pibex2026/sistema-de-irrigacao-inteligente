a7e7cdd7b6fcfdb167cfdba1f2f2beb09b4bd41f
victor.giuliano2162 <vitor.giuliano@gmail.com>
2026-08-05 08:54:30 -0300

    (create): orchestrator

---
+++ embedded/include/orchestrator.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+/*
+    The aim of this lauyer its abstract interaction with FileLogger and SensorsManager
+    the aplication main funcion will call this class and pass, as parameter the states 
+    that will lead program flow
+*/
